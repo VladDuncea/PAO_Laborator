a67288332f9cd377220110538a9127940e24351d
VladDuncea <vlad_duncea_31@yahoo.com>
2020-03-11 22:48:33 +0200

    Finalizat Matrice si Maximum

--- src/vlad/duncea/lab3/Matrice.java
+++ src/vlad/duncea/lab3/Matrice.java
@@ -3,7 +3,7 @@ package vlad.duncea.lab3;
 public class Matrice {
 
     private int nrl,nrc;
-    public int data[][];
+    public int[][] data;
 
     Matrice(int nrl, int nrc)
     {
@@ -18,11 +18,7 @@ public class Matrice {
         this.nrl = m.nrl;
 
         data = new int[nrl][nrc];
-        for(int i =0;i<nrl;i++)
-            for(int j =0;j<nrc;j++)
-            {
-                data[i][j] = m.data[i][j];
-            }
+        data = m.data;
     }
 
     public Matrice add(Matrice m)
@@ -60,9 +56,44 @@ public class Matrice {
         return result;
     }
 
+    public String toString()
+    {
+        String print = "NRL: "+nrl +"\nNRC: "+nrc+"\n";
+        for(int i =0;i<nrl;i++)
+        {
+            for(int j =0;j<nrc;j++)
+            {
+                print += data[i][j] + " ";
+            }
+            print+="\n";
+        }
+
+        return print;
+    }
+
     public static void main(String[] args)
     {
+        Matrice m1,m2;
+        m1= new Matrice(3,3);
 
+        m1.data[0][0]=1;
+        m1.data[0][1]=2;
+        m1.data[0][2]=3;
+        m1.data[1][0]=4;
+        m1.data[1][1]=5;
+        m1.data[1][2]=6;
+        m1.data[2][0]=7;
+        m1.data[2][1]=8;
+        m1.data[2][2]=9;
+        m2= new Matrice(m1);
+        System.out.println("m2:(identic cu m1)");
+        System.out.println(m2.toString());
+        System.out.println("m1+m2");
+        System.out.println(m1.add(m2));
+        System.out.println("m1*m2");
+        System.out.println(m1.multiply(m2));
+        System.out.println("m1^3");
+        System.out.println(m1.pow(3));
 
     }
 }
---
+++ src/vlad/duncea/lab3/Maximum.java
@@ -0,0 +1,51 @@
+package vlad.duncea.lab3;
+
+public class Maximum {
+
+    int nr;
+
+    public Maximum()
+    {
+        this(0);
+    }
+
+    public Maximum(int nr)
+    {
+        this.nr = nr;
+    }
+
+    public int maxim(int a)
+    {
+        return a >nr ? a:nr;
+    }
+
+    //Supraincarcarea
+    public int maxim(int a, int b)
+    {
+        int c = nr;
+        int rez;
+        nr = maxim(b);
+        rez = maxim(a);
+        nr = c;
+        return rez;
+    }
+
+    public int maxim(int a,int b, int c)
+    {
+        return maxim(maxim(a,b),maxim(c));
+    }
+
+    public int maxim(int a,int b,int c,int d)
+    {
+        return maxim(maxim(a,b,c),maxim(d));
+    }
+
+    public static void main(String[] args)
+    {
+        Maximum m = new Maximum(5);
+        System.out.println(m.maxim(10));
+        System.out.println(m.maxim(3,4));
+        System.out.println(m.maxim(9,2,3));
+        System.out.println(m.maxim(-4,2,3,-10));
+    }
+}
